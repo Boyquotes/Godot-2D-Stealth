e05b4c74613f0d78e8667d238265257c9a42469d
RahulKA-RAE <karahul209@gmail.com>
2021-10-21 00:31:07 +0530

    Initial commit

---
+++ Scripts/CharacterTemplate.gd
@@ -0,0 +1,8 @@
+extends KinematicBody2D
+
+#Velocity
+const SPEED=10
+#Max velocity
+const MAX_SPEED=100
+#Time taken to go from moving to dead halt
+const STOPPING_TIME=0.2
---
+++ Scripts/Player.gd
@@ -0,0 +1,23 @@
+extends "res://Scripts/CharacterTemplate.gd"
+
+var Motion=Vector2(0,0)
+
+func UpdateMovement():
+	look_at(get_global_mouse_position())
+	if Input.is_action_pressed("move_up") and not Input.is_action_pressed("move_down"):
+		Motion.y=clamp(Motion.y-SPEED,-MAX_SPEED,0)  
+	elif Input.is_action_pressed("move_down") and not Input.is_action_pressed("move_up"):
+		Motion.y=clamp(Motion.y+SPEED,0,MAX_SPEED)
+	else:
+		Motion.y=lerp(Motion.y,0,STOPPING_TIME)
+		
+	if Input.is_action_pressed("move_right") and not Input.is_action_pressed("move_left"):
+		Motion.x=clamp(Motion.x+SPEED,0,MAX_SPEED)
+	elif Input.is_action_pressed("move_left") and not Input.is_action_pressed("move_right"):
+		Motion.x=clamp(Motion.x-SPEED,-MAX_SPEED,0)
+	else:
+		Motion.x=lerp(Motion.x,0,STOPPING_TIME)
+	
+func  _physics_process(delta):
+	UpdateMovement()
+	move_and_slide(Motion)
